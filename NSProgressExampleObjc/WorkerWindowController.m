40a1fd9649dd6c1d6f484d420c1f09e72ea72247
jaanus <jaanus@gmail.com>
2015-01-24 20:55:25 +0200

    Children also show progress.

--- NSProgressExampleObjc/WorkerWindowController.m
+++ NSProgressExampleObjc/WorkerWindowController.m
@@ -15,6 +15,7 @@
 @property (weak) IBOutlet NSTextField *statusLabel;
 @property (strong) IBOutlet NSWindow *progressSheet;
 @property (weak) IBOutlet NSProgressIndicator *progressIndicator;
+@property (strong, nonatomic) NSProgress *taskProgress;
 
 @end
 
@@ -37,11 +38,9 @@
 
 - (void)doWork
 {
+    // Set up the UI: update status text and present the sheet.
     self.statusLabel.stringValue = @"Doing something…";
-
-    // Present the indeterminate progress sheet
     [self.window beginSheet:self.progressSheet completionHandler:nil];
-    [self.progressIndicator startAnimation:self];
     
     // Run the task
     [self longComputationTask];
@@ -49,37 +48,55 @@
 
 - (void)longComputationTask
 {
-    /// One iteration length in seconds
+    /// One iteration length in seconds. Smaller lengths seem to be more crashy.
     CGFloat iterationLength = 0.05f;
     
     NSInteger iterationCount = self.taskDuration / iterationLength;
     
-    NSProgress *taskProgress = [NSProgress progressWithTotalUnitCount:iterationCount];
-    taskProgress.cancellationHandler = ^{
-        [self longTaskIsDone:NO];
+    // Set up the progress reporter. There may or may not be any upstream progress objects,
+    // we don’t need to care here.
+    self.taskProgress = [NSProgress progressWithTotalUnitCount:iterationCount];
+
+    // If the upstream signals cancellation, this handler is called.
+    __weak typeof(self) weakSelf = self;
+    self.taskProgress.cancellationHandler = ^{
+        [weakSelf longComputationTaskIsDone:NO];
     };
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         for (NSInteger i = 0; i < iterationCount; i++) {
-            [NSThread sleepForTimeInterval:iterationLength];
-            taskProgress.completedUnitCount++;
 
-//            Log some info in the console if you want to be fancy.
-//            NSLog(@"worker %@, iteration %ld of %ld", self, (long)i, (long)iterationCount);
+            // If we happened to be cancelled from upstream, don’t do any more work,
+            // break out of the block.
+            if (self.taskProgress.cancelled) { return; }
+
+            // Simulate hard work and increment the count when one unit is done.
+            [NSThread sleepForTimeInterval:iterationLength];
+            self.taskProgress.completedUnitCount++;
+
+            // If you want, uncomment this to log each iteration tn the console.
+            // NSLog(@"worker %@, iteration %ld of %ld", self, (long)i, (long)iterationCount);
         }
 
-        // If we are cancelled when the task is done, we no longer want to be called
-        taskProgress.cancellationHandler = nil;
+        // If we are cancelled after the task is done, we’ll ignore the cancellation
+        self.taskProgress.cancellationHandler = nil;
         
-        [self longTaskIsDone:YES];
+        // After we’re done, signal our completion handler.
+        [self longComputationTaskIsDone:YES];
     });
 }
 
-- (void)longTaskIsDone:(BOOL)completed
+/// Completion handler. Pass NO if this was a cancellation and YES if computation completed successfully.
+- (void)longComputationTaskIsDone:(BOOL)completed
 {
+    // Don’t know which queue called us, so schedule back to main just in case.
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self.progressIndicator stopAnimation:self];
+        
+        // No matter if we were cancelled or ended naturally,
+        // we should tear down the sheet UI.
         [self.window endSheet:self.progressSheet];
+        
+        // Set message based on whether we completed or were cancelled.
         if (completed) {
             self.statusLabel.stringValue = @"The answer is 42.";
         } else {
